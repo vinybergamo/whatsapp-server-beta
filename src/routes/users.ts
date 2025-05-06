fc4661d038f96c3d275ed5a310df8948e6b232f5
vinybergamo <vinybergamo@gmail.com>
2025-05-06 14:25:23 -0300

    Adiciona autenticação de usuário e rota para obter informações do usuário logado

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,3 +1,12 @@
 import { FastifyInstance } from "fastify";
+import { userAuthPlugin } from "../helpers/plugins/user-auth";
 
-export function usersRoute(app: FastifyInstance) {}
+export function usersRoute(app: FastifyInstance) {
+  app.register(userAuthPlugin);
+
+  app.get("/me", async (request, reply) => {
+    const user = request.user;
+
+    return reply.code(200).send(user);
+  });
+}
