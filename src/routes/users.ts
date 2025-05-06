11751961b5ab584ee72618989918a931206c5751
vinybergamo <vinybergamo@gmail.com>
2025-05-06 14:27:20 -0300

    Altera rota de obtenção de informações do usuário logado para '/users/me'

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,7 +4,7 @@ import { userAuthPlugin } from "../helpers/plugins/user-auth";
 export function usersRoute(app: FastifyInstance) {
   app.register(userAuthPlugin);
 
-  app.get("/me", async (request, reply) => {
+  app.get("/users/me", async (request, reply) => {
     const user = request.user;
 
     return reply.code(200).send(user);
